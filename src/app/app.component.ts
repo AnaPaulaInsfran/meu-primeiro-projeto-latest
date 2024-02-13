a775bde775a56eaee664db8b09a60d410dfe8344
AnaInsfran <anainsfran@gmail.com>
2024-02-13 15:08:26 -0300

    feat:final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
