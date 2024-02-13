860f87871c6ae33519293c6e5f1c2cd286d1196d
AnaInsfran <anainsfran@gmail.com>
2024-02-13 14:45:54 -0300

    feat: entendendo o componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { routes } from './app.routes';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet],
+  template: `
+    <!-- <router-outlet></router-outlet> -->
+    <h1>Curso de Angular</h1>
+    <h2>Components</h2>
+    `,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
 }
+
