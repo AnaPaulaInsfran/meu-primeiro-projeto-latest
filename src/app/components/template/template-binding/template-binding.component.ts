4714a5c7f5f143c337a52ceb64ca2cee8e862d34
AnaInsfran <anainsfran@gmail.com>
2024-02-13 18:30:51 -0300

    feat: Template - property bindingfeat: Template - property binding

--- src/app/components/template/template-binding/template-binding.component.ts
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -13,6 +13,16 @@ export class TemplateBindingComponent {
   public name = 'Ana Insfran';
   public age = 35;
   public condition = this.age < 1 ? "Teste" : "Teste2";
+  public isDisabled = false;
+  public srcValue =
+    'https://vidafullstack.com.br/wp-content/uploads/2022/04/Aprenda-o-que-e-Databinding-com-Angular-1080x675.jpg';
+
+  // constructor(){
+  //   setTimeout( () => {
+  //     this.name = "Graziela"
+  //   },1000)
+  // }
+
   //método
   public sum(val1: number, val2: number) {
     return val1 + val2;
