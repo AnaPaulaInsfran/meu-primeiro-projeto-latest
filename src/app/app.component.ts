a9ad69bc9883b984c2fd0c1b3e53d52b1d55704c
AnaInsfran <anainsfran@gmail.com>
2024-02-21 21:14:49 -0300

    alteração app.components - voltando sem alteração,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { routes } from './app.routes';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 //Components
 import { NewComponent } from './components/new-component/new-component.component';
-import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent],
   template: `
     <h1>Curso de Angular</h1>
-    <app-template-binding />
+    <h2>Template</h2>
+    <h3>Text Interpolation</h3>
     `
 })
 export class AppComponent {
