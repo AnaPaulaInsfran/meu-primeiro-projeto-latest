55ce16979d7ce919a6123307f584e10c85fd2abb
AnaInsfran <anainsfran@gmail.com>
2024-02-26 00:40:20 -0300

    feat:usado-signal

---
+++ src/app/components/signals/signals.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './signals.component.html',
+  styleUrl: './signals.component.scss'
+})
+export class SignalsComponent {
+  public firstName =signal('Ana');
+  public lastName = signal('Insfran');
+
+  public fullName = computed( () => {
+    return this.firstName() + this.lastName();
+  })
+
+  public array = signal([1]);
+
+  constructor() {
+
+  }
+
+  public updateName(){
+    return this.firstName.set('Paula')
+  }
+
+  public updateArray() {
+    this.array.update(
+      (oldValue: Array<number>) => {
+        console.log(oldValue)
+        return [...oldValue, oldValue.length + 1]
+      }
+    )
+  }
+}
