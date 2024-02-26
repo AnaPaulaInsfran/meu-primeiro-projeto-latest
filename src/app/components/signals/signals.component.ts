aa0204465323e499a44d3db45a33eba9f66f1cf3
AnaInsfran <anainsfran@gmail.com>
2024-02-26 00:53:30 -0300

    feat:effect

--- src/app/components/signals/signals.component.ts
+++ src/app/components/signals/signals.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
@@ -13,25 +13,46 @@ export class SignalsComponent {
   public lastName = signal('Insfran');
 
   public fullName = computed( () => {
-    return this.firstName() + this.lastName();
+    return this.firstName() + ' ' + this.lastName();
   })
 
   public array = signal([1]);
 
-  constructor() {
 
-  }
+  /*
+  effect - raramente são necessários na maioria dos códigos,
+  mas podem ser úteis em circuntãncias específicas.
+  - Registro de dados sendo exibidos e quando eles mudam, seja para análise
+  ou como ferramenta de depuração.
 
-  public updateName(){
-    return this.firstName.set('Paula')
-  }
+  - Manter os dados sincronizados com o window.localStorage.
 
-  public updateArray() {
-    this.array.update(
-      (oldValue: Array<number>) => {
-        console.log(oldValue)
-        return [...oldValue, oldValue.length + 1]
-      }
-    )
-  }
+  - Adicionano comportamento DOM personalizado que não pode ser expresso com
+  sintaxe de modelo.
+
+  - Executar renderizção personalizada em um <canvas>, biblioteca de gráficos
+  ou outra biblioteca de UI de terceiros.
+  */
+
+ constructor() {
+  effect(() => {
+    console.log(this.firstName())
+    console.log(this.array())
+  })
+ }
+
+ public updateName(){
+   return this.firstName.set('Paula')
+ }
+
+ public updateArray() {
+   this.array.update(
+     (oldValue: Array<number>) => {
+       console.log(oldValue)
+       return [...oldValue, oldValue.length + 1]
+     }
+   )
+ }
 }
+
+
